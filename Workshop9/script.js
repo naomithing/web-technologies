c8460315bf832630855c06e2a0cc5a203a5adbf7
naomithing <np03cs4a220491@heraldcollege.edu.np>
2023-02-11 21:12:31 +0545

    adding w9

---
+++ Workshop9/script.js
@@ -0,0 +1,51 @@
+//console.log("Hello world. I am learning JavaScript")
+/*let a = 6;
+console.log(typeof(a));
+let b = 8;
+let sum = a+b;
+console.log(sum);*/
+
+/*var a = 12;
+var b = 2;
+var product = a*b;
+console.log(product)
+
+const PI = 3.14;
+let radius = window.prompt("enter radius");
+let circumference= 2 * PI *radius;
+console.log("the circumference of a circle is: " +circumference);*/
+
+
+// function bark(){
+//     console.log("the dog is barking");
+// }
+// // bark();
+
+// var age=window.prompt("enter the age");
+// function age(){
+//     if (age<12){
+//         console.log("the dog is young");
+//     } else {
+//         console.log("the dog is old");
+//     }
+// }
+// function viraj(){
+    
+//         if (age<12){
+//             console.log("he is a baby");
+//         } else {
+//             console.log("he is a teen");
+//         }
+//     }
+//     age();
+//     viraj();
+
+document.getElementById("myBtn").onclick = function(){
+    let radius = document. ("myNumber").value;
+    console.log(radius);
+    const PI = 3.14;
+    let circumference = 2 * PI * radius;
+    document.getElementById("circumference").innerHTML = "Circumference";
+//  document.getElementById("circumference").append(circumference);
+ alert("Hi Hello Annyeong")
+}
